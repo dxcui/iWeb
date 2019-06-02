830b4fcd4a49bfb07182efa0e7b5c15e5aa030bf
dxcui <1061918348@qq.com>
2019-06-02 18:35:07 +0800

    更改Jenkinsfile_master的jdk和maven环境

--- jenkinsfile_master.groovy
+++ jenkinsfile_master.groovy
@@ -10,8 +10,8 @@ stage('maven compile & package') {
         sh ". ~/.bash_profile"
 
         //定义maven java环境
-        def mvnHome = tool 'maven-3.6.0_master'
-        def jdkHome = tool 'jdk1.8_master'
+        def mvnHome = tool 'maven3.6'
+        def jdkHome = tool 'jdk1.8'
         env.PATH = "${mvnHome}/bin:${env.PATH}"
         env.PATH = "${jdkHome}/bin:${env.PATH}"
         sh "mvn clean install"
